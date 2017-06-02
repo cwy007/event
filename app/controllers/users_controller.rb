ac5d415669d3596da8b0bd0f2b248bd3033b003a
chanweiyan <chanweiyan007@gmail.com>
2017-06-02 22:21:26 +0800

    profile show function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
-    @user = current_user
     if @user.profile.nil?
       @user.create_profile
     end
   end
 
   def update
-    @user = current_user
 
     if @user.update(user_params)
       flash[:notice] = "修改成功"
@@ -20,6 +22,11 @@ class UsersController < ApplicationController
   end
 
   protected
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+  end 
 
   def user_params
     params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty])
