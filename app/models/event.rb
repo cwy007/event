1fdde9f65d00c5f838188badacd01b88ef23158c
chanweiyan <chanweiyan007@gmail.com>
2017-06-02 17:58:11 +0800

    emplement category CRUD

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,11 @@ class Event < ApplicationRecord
 
  before_validation :generate_friendly_id, :on => :create
 
+belongs_to :category, :optional => true
+
  STATUS = ["draft", "public", "private"]
  validates_inclusion_of :status, :in => STATUS
- 
+
  def to_param
    self.friendly_id
  end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root "events#index"
     resources :events
+    resources :categories
   end
 
   resource :user
-  
+
   root "events#index"
 
 end
