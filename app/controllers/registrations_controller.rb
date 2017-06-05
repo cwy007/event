31103e2bd636b1f1c0d465139f9e6a79e22667b6
chanweiyan <chanweiyan007@gmail.com>
2017-06-05 19:07:58 +0800

    implement wizards step1

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,11 +7,11 @@ class RegistrationsController < ApplicationController
   def create
     @registration = @event.registrations.new(registration_params)
     @registration.ticket = @event.tickets.find( params[:registration][:ticket_id] )
-    @registration.status = "confirmed"
+    @registration.status = "pending"
     @registration.user = current_user
 
     if @registration.save
-      redirect_to event_registration_path(@event, @registration)
+      redirect_to step2_registration_path(@event, @registration)
     else
       render "new"
     end
@@ -30,5 +30,5 @@ class RegistrationsController < ApplicationController
   def find_event
     @event = Event.find_by_friendly_id(params[:event_id])
   end
-  
+
 end
