0322936622bf63e0eff9faea395dbac455a2b39a
chanweiyan <chanweiyan007@gmail.com>
2017-06-06 23:27:19 +0800

    analyse daily registrations & sort by registration status

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -51,15 +51,6 @@ class Admin::EventsController < AdminController
        end
      }
 
-    #  @data1 = {
-    #      labels: ticket_names,
-    #      datasets: [{
-    #        label: "# of Registrations",
-    #        data: @event.tickets.map{ |t| t.registrations.count },
-    #        backgroundColor: colors,
-    #        borderWidth: 1
-    #      }]
-    #  }
 
      @data2 = {
        labels: ticket_names,
@@ -70,6 +61,23 @@ class Admin::EventsController < AdminController
          borderWidth: 1
          }]
      }
+
+     if @event.registrations.any?
+       dates = (@event.registrations.order("id ASC").first.created_at.to_date..Date.today).to_a
+
+       @data3 = {
+         labels: dates,
+         datasets: Registration::STATUS.map do |s|
+           {
+             :label => I18n.t(s, :scope => "registration.status"),
+             :data => dates.map{ |d|
+               @event.registrations.by_status(s).where("created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count
+             },
+             borderColor: status_colors[s]
+           }
+         end
+       }
+     end
 
   end
 
